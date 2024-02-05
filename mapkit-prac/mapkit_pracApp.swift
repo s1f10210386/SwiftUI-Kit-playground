3fb78e68b5e3ace86af88b8286e282610516c4aa
hotaka <s1f102103864@iniad.org>
2024-02-05 19:28:38 +0900

    Initial Commit

---
+++ mapkit-prac/mapkit_pracApp.swift
@@ -0,0 +1,17 @@
+//
+//  mapkit_pracApp.swift
+//  mapkit-prac
+//
+//  Created by 金澤帆高 on 2024/02/05.
+//
+
+import SwiftUI
+
+@main
+struct mapkit_pracApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
