cb51bf7a1dc77a70c300082d9d0f514525bd0bf9
hotaka <s1f102103864@iniad.org>
2024-02-06 22:10:25 +0900

    feat : Add FirebaseAuth

---
+++ mapkit-prac/Views/ResetPasswordView.swift
@@ -0,0 +1,18 @@
+//
+//  ResetPasswordView.swift
+//  mapkit-prac
+//
+//  Created by 金澤帆高 on 2024/02/06.
+//
+
+import SwiftUI
+
+struct ResetPasswordView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ResetPasswordView()
+}
---
+++ mapkit-prac/Views/SignInView.swift
@@ -0,0 +1,18 @@
+//
+//  SignInView.swift
+//  mapkit-prac
+//
+//  Created by 金澤帆高 on 2024/02/06.
+//
+
+import SwiftUI
+
+struct SignInView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignInView()
+}
