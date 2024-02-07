f19b8ec552f1638a3d512f4c71577dc3625390e8
hotaka <s1f102103864@iniad.org>
2024-02-08 00:17:29 +0900

    feat : firestore

---
+++ mapkit-prac/Views/TextView.swift
@@ -0,0 +1,18 @@
+//
+//  TextView.swift
+//  mapkit-prac
+//
+//  Created by 金澤帆高 on 2024/02/07.
+//
+
+import SwiftUI
+
+struct TextView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TextView()
+}
