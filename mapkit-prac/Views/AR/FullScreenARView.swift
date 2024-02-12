72285c79cc7147b91d6b8f378ab677d8be643c87
hotaka <s1f102103864@iniad.org>
2024-02-13 03:55:44 +0900

    fix

--- mapkit-prac/Views/AR/FullScreenARView.swift
+++ mapkit-prac/Views/AR/FullScreenARView.swift
@@ -2,34 +2,14 @@
 //  FullScreenARView.swift
 //  mapkit-prac
 //
-//  Created by 金澤帆高 on 2024/02/07.
+//  Created by 金澤帆高 on 2024/02/13.
 //
 
 import SwiftUI
 
 struct FullScreenARView: View {
-    @Environment(\.presentationMode) var presentationMode
-
     var body: some View {
-        VStack {
-            // ここにARViewContainerを配置
-            ARViewContainer()
-                .edgesIgnoringSafeArea(.all) // フルスクリーン表示をする場合、Safe Areaを無視する
-
-            // 閉じるボタン
-            Button(action: {
-                // ビューを閉じる
-                presentationMode.wrappedValue.dismiss()
-            }) {
-                Image(systemName: "xmark.circle.fill")
-                    .padding()
-                    .foregroundColor(.white)
-                    .background(Color.black.opacity(0.7))
-                    .clipShape(Circle())
-            }
-            .padding() // 必要に応じて調整
-            .accessibilityLabel(Text("閉じる"))
-        }
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
