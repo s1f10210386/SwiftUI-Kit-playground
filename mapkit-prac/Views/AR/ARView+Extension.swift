b225fa4a6256280b1d008442fc31edde8c71b870
hotaka <s1f102103864@iniad.org>
2024-02-13 20:34:17 +0900

    fix: オブジェクトをハードコードで出力できるところに戻した

---
+++ mapkit-prac/Views/AR/ARView+Extension.swift
@@ -0,0 +1,18 @@
+//
+//  ARView+Extension.swift
+//  mapkit-prac
+//
+//  Created by 金澤帆高 on 2024/02/13.
+//
+
+import SwiftUI
+
+struct ARView_Extension: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ARView_Extension()
+}
