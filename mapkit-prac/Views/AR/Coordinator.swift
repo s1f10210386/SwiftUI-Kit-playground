1764681084615d8ca93030cdc83905db9e8ec0b2
hotaka <s1f102103864@iniad.org>
2024-02-13 20:09:47 +0900

    Coordinatorから引っ張ってこようとしてるがクラッシュ

---
+++ mapkit-prac/Views/AR/Coordinator.swift
@@ -0,0 +1,18 @@
+//
+//  Coordinator.swift
+//  mapkit-prac
+//
+//  Created by 金澤帆高 on 2024/02/13.
+//
+
+import SwiftUI
+
+struct Coordinator: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Coordinator()
+}
