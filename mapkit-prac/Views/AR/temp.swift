11b7febe3597a2ba0689d543ece6f79dddbcf271
hotaka <s1f102103864@iniad.org>
2024-02-14 19:18:00 +0900

    temp commit

---
+++ mapkit-prac/Views/AR/temp.swift
@@ -0,0 +1,18 @@
+//
+//  temp.swift
+//  mapkit-prac
+//
+//  Created by 金澤帆高 on 2024/02/14.
+//
+
+import SwiftUI
+
+struct temp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    temp()
+}
