6a114650d11dbcf648d4c602455494d30ce513f0
hotaka <s1f102103864@iniad.org>
2024-02-16 07:48:16 +0900

    feat: ダグラスピュータアルゴリズム

--- mapkit-prac/Views/Map/LocationVM+Extension.swift
+++ mapkit-prac/Views/Map/LocationVM+Extension.swift
@@ -6,7 +6,8 @@
 //
 
 import MapKit
-
+import Foundation
+import CoreLocation
 
 
 extension LocationViewModel {
@@ -45,14 +46,51 @@ extension LocationViewModel {
         var coordinates = [CLLocationCoordinate2D](repeating: kCLLocationCoordinate2DInvalid, count: pointCount)
         polyline.getCoordinates(&coordinates, range: NSRange(location: 0, length: pointCount)) //ポリラインを形成するすべての点の座標をこの配列にコピーする
         
-        self.coordinates = coordinates // ViewModelの座標配列を更新
+        // ダグラスピューターアルゴリズムを適用して座標を単純化
+        let simplifiedCoordinates = douglasPeuckerAlgorithm(points: coordinates, epsilon: 0.0001)
         
-        //        self.coordinates.forEach { coordinate in
-        //            print("緯度: \(coordinate.latitude), 経度: \(coordinate.longitude)")
-        //        }
-        
-        
-        
-        
+        // ViewModelの座標配列を更新
+        self.coordinates = simplifiedCoordinates
     }
+
+    func douglasPeuckerAlgorithm(points: [CLLocationCoordinate2D], epsilon: Double) -> [CLLocationCoordinate2D] {
+        // ポイントが2つ以下の場合はそのまま返す
+        guard points.count > 2 else {
+            return points
+        }
+        
+        var maxDistance = 0.0
+        var index = 0
+        for i in 1..<points.count - 1 {
+            let distance = perpendicularDistance(from: points[i], toLineFormedBy: points.first!, and: points.last!)
+            if distance > maxDistance {
+                index = i
+                maxDistance = distance
+            }
+        }
+        
+        // 最大距離がepsilonより小さい場合、ポイントを間引く
+        if maxDistance < epsilon {
+            return [points.first!, points.last!]
+        }
+        
+        // 再帰的に処理
+        let leftRecursiveResult = douglasPeuckerAlgorithm(points: Array(points[0...index]), epsilon: epsilon)
+        let rightRecursiveResult = douglasPeuckerAlgorithm(points: Array(points[index..<points.count]), epsilon: epsilon)
+        
+        // 最初と最後のポイントが重複するのを避けるために、右側の結果から最初のポイントを除外して結合
+        return leftRecursiveResult + rightRecursiveResult.dropFirst()
+    }
+
+    func perpendicularDistance(from point: CLLocationCoordinate2D, toLineFormedBy pointA: CLLocationCoordinate2D, and pointB: CLLocationCoordinate2D) -> Double {
+        let A = pointB.latitude - pointA.latitude
+        let B = pointA.longitude - pointB.longitude
+        let C = pointB.longitude * pointA.latitude - pointA.longitude * pointB.latitude
+
+        // 点 (point.latitude, point.longitude) と直線 Ax + By + C = 0 との距離
+        let distance = abs(A * point.longitude + B * point.latitude + C) / sqrt(A * A + B * B)
+        return distance
+    }
+
+
 }
